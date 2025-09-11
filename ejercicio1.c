c107e8b23cd3e9a6d35db237cd2396f8f5884a25
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-09-11 13:57:25 -0700

    ejercicio #1 Practica #3

--- ejercicio1.c
+++ ejercicio1.c
@@ -4,39 +4,41 @@ int main() {
     int palo, valor;
 
     for (palo = 1; palo <= 4; palo++) {
+        switch (palo) {
+            case 1:
+                printf("=== Corazones ===\n");
+                break;
+            case 2:
+                printf("=== Diamantes ===\n");
+                break;
+            case 3:
+                printf("=== Treboles ===\n");
+                break;
+            case 4:
+                printf("=== Picas ===\n");
+                break;
+        }
         for (valor = 1; valor <= 13; valor++) {
+
             switch (valor) {
                 case 1:
-                    printf("A");
+                    printf("A ");
                     break;
                 case 11:
-                    printf("J");
+                    printf("J ");
                     break;
                 case 12:
-                    printf("Q");
+                    printf("Q ");
                     break;
                 case 13:
-                    printf("K");
+                    printf("K ");
                     break;
                 default:
-                    printf("%d", valor);
-                    break;
-            }
-            switch (palo) {
-                case 1:
-                    printf("Corazones\n");
-                    break;
-                case 2:
-                    printf("Diamantes\n");
-                    break;
-                case 3:
-                    printf("Treboles\n");
-                    break;
-                case 4:
-                    printf("Picas\n");
+                    printf("%d ", valor);
                     break;
             }
         }
+
     }
 
     return 0;
