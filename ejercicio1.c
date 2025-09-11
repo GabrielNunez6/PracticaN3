44b99750b6b8df82c28c2cbd1e915ef1b88cb015
GabrielNunez6 <gabriel.nunez.saldana@uabc.edu.mx>
2025-09-11 13:47:42 -0700

    ejercicio #1 Practica #3

---
+++ ejercicio1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int palo, valor;
+
+    for (palo = 1; palo <= 4; palo++) {
+        for (valor = 1; valor <= 13; valor++) {
+            switch (valor) {
+                case 1:
+                    printf("A");
+                    break;
+                case 11:
+                    printf("J");
+                    break;
+                case 12:
+                    printf("Q");
+                    break;
+                case 13:
+                    printf("K");
+                    break;
+                default:
+                    printf("%d", valor);
+                    break;
+            }
+            switch (palo) {
+                case 1:
+                    printf("Corazones\n");
+                    break;
+                case 2:
+                    printf("Diamantes\n");
+                    break;
+                case 3:
+                    printf("Treboles\n");
+                    break;
+                case 4:
+                    printf("Picas\n");
+                    break;
+            }
+        }
+    }
+
+    return 0;
+}
